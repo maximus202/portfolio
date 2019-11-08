3d6f40524c906b2392a9a98294de6500e4019fc5
Max R <maxx.ratto@gmail.com>
2019-11-08 06:41:33 -0700

    Added document ready function, function stubs for each user story with psuedocode, a function to run event handlers, a js listener for when the page loads and makes the event handlers function run.

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-//User story: The user needs to be able to see the projects menu where it'll display all the projects.
+$(document).ready(function () {
+    function displayProjectsMenu() {
+        console.log('displayProjectsMenu() ran');
+        //User story: The user needs to be able to see the projects menu where it'll display all the projects.
+    }
 
-//User story: The user clicks on a project to see more details about it.
+    function displayProjectDetailsButton() {
+        console.log('displayProjectDetailsButton() ran');
+        //User story: The user clicks on a project to see more details about it.
+    }
 
-//User story: The user is presented with a screen that shows details of the project they're viewing.
+    function displayProjectDetailsScreen() {
+        console.log('displayProjectDetailsScreen() ran');
+        //User story: The user is presented with a screen that shows details of the project they're viewing.
+    }
 
-//User story: The user clicks to see a render of the project or the code.
+    function displaySeeCodeButton() {
+        console.log('displaySeeCodeButton() ran');
+        //User story: The user clicks to see a render of the project.
+    }
 
-//User story: The user is able to return to the home screen of the site.
+    function displaySeeDemoButton() {
+        console.log('displayDemoButton() ran');
+        //User story: The user clicks to see the code for the project they're viewing.
+    }
+
+    function displayBackButton() {
+        console.log('displayBackButton() ran');
+        //User story: The user is able to return to the home screen of the site.
+    }
+
+    function setUpEventHandlers() {
+        //Runs event handlers at page load.
+        console.log('setUpEventHandlers() ran');
+        displayProjectsMenu();
+        displayProjectDetailsButton();
+        displayProjectDetailsScreen();
+        displaySeeCodeButton();
+        displaySeeDemoButton();
+        displayBackButton();
+    }
+    //Runs setUpEventHandlers() at page load.
+    $(setUpEventHandlers())
+});
