2d7184bbfc3a085e614daf16fabcaa973aded359
Max R <maxx.ratto@gmail.com>
2019-11-08 06:24:31 -0700

    Added user stories to js file.

---
+++ index.js
@@ -0,0 +1,9 @@
+//User story: The user needs to be able to see the projects menu where it'll display all the projects.
+
+//User story: The user clicks on a project to see more details about it.
+
+//User story: The user is presented with a screen that shows details of the project they're viewing.
+
+//User story: The user clicks to see a render of the project or the code.
+
+//User story: The user is able to return to the home screen of the site.
